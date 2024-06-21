d44e6207dbab57d7208dcdb5b5b59debb6e37542
Gustavo Wazlawick <eva01.gustavo@gmail.com>
2024-06-21 11:54:59 -0300

    tentando fazer 1482 do leetcode

---
+++ lc01.ts
@@ -0,0 +1,44 @@
+function minDays(bloomDay: number[], m: number, k: number): number {
+    const flowerDay: number[][] = [];
+    const bouquets: number[] = [];
+    let bqt: number = 0
+    let stop = 0
+
+    for (let i = 0; i < bloomDay.length; i++) {
+        stop = 0
+        try {
+            if (i = i + 1) {
+                for (let j = 0; bloomDay[j] === bloomDay[i]; j++) {
+                    while (bloomDay[j] === bloomDay[i]) {
+                        flowerDay.push([]);
+                        flowerDay[flowerDay.length - 1].push(bloomDay[j]);
+                        console.log(flowerDay);
+                        j++
+                    }
+                    if (flowerDay[j].length == k) {
+                        bouquets.push(bloomDay[i])
+                        i = j
+                    } else {
+                        i = j
+                    }
+                } else {
+                    
+                }
+            }
+        } catch (error) {
+            console.log("deu merda kkkk");
+            break;
+        }
+        bqt = Math.max(...bouquets)
+        if (bqt == m) {
+            break;
+        } else if (i == bloomDay.length - 1 && bqt != m) {
+            bqt = -1
+        } else {
+            console.log("deu merda");
+        }
+    }
+    return bqt
+};
+
+console.log(minDays([7, 7, 7, 7, 12, 7, 7], 2, 3));
